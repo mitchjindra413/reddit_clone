02dc498f4dc50947ecde490495a3597d649a6172
“Vince <vmemmo1@gmail.com>
2022-09-23 14:13:57 -0700

    User Auth

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,21 @@
+# == Schema Information
+#
+# Table name: users
+#
+#  id              :integer          not null, primary key
+#  username        :string           not null
+#  session_token   :string           not null
+#  password_digest :string           not null
+#  created_at      :datetime         not null
+#  updated_at      :datetime         not null
+#
 class User < ApplicationRecord
     before_validation :ensure_session_token
+
+    validates :username, :password_digest, :session_token, presence:true
+    validates :username, :session_token, uniqueness:true
+
+    validates :password, length: {minimum:8 }, allow_nil:true
 
     attr_reader :password
 
